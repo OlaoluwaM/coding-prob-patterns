b477742ed72ae56ae2789b918538357e79330b0f
OlaoluwaM <37044906+OlaoluwaM@users.noreply.github.com>
2022-03-05 07:15:37 +0100

    chore: Moved patterns to a 'patterns' directory

--- pattern-folder-setup.sh
+++ pattern-folder-setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 patternName="$1"
+test -z $patternName && echo "Please specify a pattern name" && exit 130
+
 rootDir=$(dirname $(realpath 0))
 
 echo "Creating Directory and boilerplate files for ${patternName}...."
 
-mkdir $rootDir/$patternName
-touch $rootDir/$patternName/{README.md,index.ts,test.ts}
+mkdir $rootDir/patterns/$patternName
+touch $rootDir/patterns/$patternName/{README.md,index.ts,test.ts}
 
 echo "Done"
